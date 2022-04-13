f6fc14a443b47616ffc874bc031e30492e46bb50
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-13 13:17:49 -0400

    Created routes function and require express

---
+++ server-backend/controllers/todo.js
@@ -0,0 +1,16 @@
+/*
+    Imports Routers 
+*/
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/Todo');
+
+/*****************************************************/
+/*
+    C-R-U-D
+    Create, Read, Update, Delete
+*/
+/****************************************************/
+
+
+module.exports = router;
