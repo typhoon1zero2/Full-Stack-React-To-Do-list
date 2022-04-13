44b3b4794627b21069e6c335e3899445d2c570c8
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-13 12:34:00 -0400

    Setup middleware

--- server-backend/server.js
+++ server-backend/server.js
@@ -7,4 +7,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3001 ;
-const router = require('./controllers/todo')
+const router = require('./controllers/todo');
+const { json } = require('express');
+
+
+/*********************************
+ *  Middleware
+ *********************************/
+app.use(cors());
+app.use(express.json());
+app.use('/todo', router);
