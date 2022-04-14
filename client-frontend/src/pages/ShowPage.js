4989f4828053d5a8ce1562d1264ea5194aeb4ba3
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-14 08:23:29 -0400

    Created && Import reacts showpage

---
+++ client-frontend/src/pages/ShowPage.js
@@ -0,0 +1,11 @@
+import React , { useState , useEffect } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const ShowPage = () => {
+  return(
+    <div className='show-Page'>
+      <h1>ShowPage</h1>
+    </div>
+  )
+}
