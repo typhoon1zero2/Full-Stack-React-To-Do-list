2862b19937fd52a106a64e214e1642b78ecbfdc1
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-14 15:31:03 -0400

    Updated and modified Layout page

--- client-frontend/src/pages/layout/Layout.js
+++ client-frontend/src/pages/layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import '../../App.css';
+import { AiOutlineCheck } from "react-icons/ai";
 
 function Layout() {
     return (
         <div className="layout">
           <header>
-            <h1 className="title">To Do List</h1>
+            <h1 className="title" ><AiOutlineCheck />To Do List</h1>
           </header>
           <main>
             <Outlet />
