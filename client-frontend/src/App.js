554aaaedfa40e1e86cff6f78aead62de50895ebf
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-14 08:42:33 -0400

    Created useState and useRef hook

---
+++ client-frontend/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Nav from './components/Nav'
+import ShowPage from "./pages/ShowPage";
+import Layout from "./pages/layout/Layout";
+// import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+
+
+
+function App() {
+  const [ todos, setTodos ] = useState({});
+  const [submitTodos, setSubmitTodos] = useState(false);
+  const [buttonTodos , setButtonTodos] = useState(false);
+  const entry = useRef(null);
+  const status = useRef(null);
+    return (
+        <>
+          <main className="App">
+            <Nav />
+            <Routes>
+              <Route path="/" element={<Layout />} />
+            </Routes>
+          </main>
+        </>
+      );
+}
+
+
+
+
+
+
+export default App;
