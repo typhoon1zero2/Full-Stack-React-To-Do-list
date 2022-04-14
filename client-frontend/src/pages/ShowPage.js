2d9bff33bcfa9507c81e0e22e0218a8ea5bdfd52
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-14 08:37:20 -0400

    Created showpage function && useEffect & useParams && useNavigate Hook

--- client-frontend/src/pages/ShowPage.js
+++ client-frontend/src/pages/ShowPage.js
@@ -3,9 +3,40 @@ import { useParams, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const ShowPage = () => {
+  const { idx } =  useParams();
+
+  const [ todo, setTodo ] = useState({})
+
+  /* Navigation Hook to navigate from page -to-page*/
+  const navigate = useNavigate()
+
+  /* UseEffect collect when page mount */
+  useEffect(() => {
+    (async () => {
+      try {
+        const { fetchData } = await axios.get(`http://localhost:3001/todos/${id}`)
+        setTodo(fetchData);
+      }catch (err) {
+        console.log(err)
+      }
+    })()
+  },[])
+
+  const deletedList = async () =>{
+    try {
+      await axios.delete(`http://localhost:3001/todos/${id}`)
+    }catch (err) {
+      console.log()
+    } finally {
+      navigate(-1)
+    }
+  }
+
   return(
     <div className='show-Page'>
       <h1>ShowPage</h1>
     </div>
   )
 }
+
+export default ShowPage
